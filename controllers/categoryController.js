40066517e40fae5c7e8a68cb0d1a05b8e5aa6d9e
amir <amiresya@gmail.com>
2024-12-01 21:01:59 +0330

    update and delete category complete ..

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,6 @@
 const createValid = require("./../validators/category/categoryCreateValid")
 const { model: categoryModel } = require("./../models/Category")
+const { isValidObjectId } = require("mongoose")
 
 
 const createCategory = async (req, res) => {
@@ -29,11 +30,57 @@ const getAllCategory = async (req, res) => {
 }
 
 const deleteCategory = async (req, res) => {
+    // get id from params
+    const { id } = req.params
+
+    // is id valid
+    if (!isValidObjectId(id)) {
+        return res.status(409).json({ message: "id is not valid .." })
+    }
+
+    // find category
+    const category = await categoryModel.find({ _id: id })
+    if (category.length < 1) {
+        return res.status(404).json({ message: "category not exist .." })
+    }
+
+    await categoryModel.findOneAndDelete({ _id: id })
+    return res.json({ message: "category delete successfully .." })
 
 }
 
 const updateCategory = async (req, res) => {
+    // get id from params
+    const { id } = req.params
 
+    // is id valid
+    if (!isValidObjectId(id)) {
+        return res.status(409).json({ message: "id is not valid .." })
+    }
+
+    // find category
+    const category = await categoryModel.find({ _id: id })
+    if (category.length < 1) {
+        return res.status(404).json({ message: "category not exist .." })
+    }
+
+    // check body by fastest valid
+    const resultValid = createValid(req.body)
+
+    if (resultValid !== true) {
+        return res.status(409).json(resultValid)
+    }
+
+    // get body
+
+    const { title, href } = req.body
+
+    // update category
+    await categoryModel.findOneAndUpdate({ _id: id }, {
+        $set: { title, href }
+    })
+
+    return res.json({ message: "category update successfully .." })
 }
 
 module.exports = { createCategory, getAllCategory, deleteCategory, updateCategory }
