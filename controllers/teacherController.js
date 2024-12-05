7dbc27a52ab501d475900868b2d683c37679b3a4
amir <amiresya@gmail.com>
2024-12-05 13:29:47 +0330

    CRUD for teacher complete ..

--- controllers/teacherController.js
+++ controllers/teacherController.js
@@ -1,21 +1,76 @@
+const { isValidObjectId } = require("mongoose")
 const { model: teacherModel } = require("./../models/Teacher")
+const validator = require("./../validators/teacher/teacherCreateValid")
 
 const getAll = async (req, res) => {
+
+    const allTeacher = await teacherModel.find({}, "-__v -createdAt -updatedAt")
+    return res.json(allTeacher)
 
 }
 
 const create = async (req, res) => {
     // is valid data by fastest
-    console.log(req.body);
+    const result = validator(req.body)
+    if (result !== true) {
+        return res.status(409).json(result)
+    }
 
     await teacherModel.create(req.body)
     res.status(201).json({ message: "teacher create successfully .." })
 }
 
 const remove = async (req, res) => {
+    const { id } = req.params
+    // check valid id
+    if (id) {
+        if (!isValidObjectId(id)) {
+            return res.status(409).json({ message: "the id is not valid .." })
+        }
+    }
+
+    // is exist teacher
+    const isExist = await teacherModel.findOne({ _id: id })
+    if (!isExist) {
+        return res.status(404).json({ message: "the teacher not found .." })
+    }
+
+    // delete teacher 
+    await teacherModel.findOneAndDelete({ _id: id })
+    return res.json({ message: "teacher delete successfully .." })
 
 }
 
-const update = async (req, res) => { }
+const update = async (req, res) => {
+    const { id } = req.params
+    // check valid id
+    if (id) {
+        if (!isValidObjectId(id)) {
+            return res.status(409).json({ message: "the id is not valid .." })
+        }
+    }
+
+    // is exist teacher
+    const isExist = await teacherModel.findOne({ _id: id })
+    if (!isExist) {
+        return res.status(404).json({ message: "the teacher not found .." })
+    }
+
+
+
+    // is valid data by fastest
+    const result = validator(req.body)
+    if (result !== true) {
+        return res.status(409).json(result)
+    }
+
+    // update teacher
+    await teacherModel.findOneAndUpdate({ _id: id }, {
+        $set: req.body
+    })
+
+    return res.json({ message: "teacher update successfully .." })
+
+}
 
 module.exports = { getAll, create, remove, update }
--- validators/teacher/teacherCreateValid.js
+++ validators/teacher/teacherCreateValid.js
@@ -1,8 +1,15 @@
 const validator = require("fastest-validator")
 
-const v =new validator()
+const v = new validator()
 
-const schema = {}
+const schema = {
+    name: { type: "string", min: 3 },
+    skills: { type: "array", min: 1 },
+    profileImg: { type: "string" },
+    description: { type: "string", min: 1 },
+    social: { type: "array" },
+
+}
 
 const check = v.compile(schema)
 
