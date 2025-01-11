2f576ae7e8ca35cb2390d76046dbf109d01a6994
amir <amiresya@gmail.com>
2025-01-11 16:56:30 +0330

    create notification complete ..

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,5 +1,25 @@
+const { isValidObjectId } = require("mongoose")
+const userModel = require("./../models/User")
+const { model: notificationModel } = require("./../models/Notification")
+
 const create = async (req, res) => {
-    // code
+    const { message, adminID } = req.body
+
+    // validation for id
+    if (!isValidObjectId(adminID)) {
+        return res.status(409).json({ message: "id not valid .." })
+    }
+
+    // the user is exist and is admin
+
+    const adminFind = await userModel.find({ _id: adminID, role: "ADMIN" })
+
+    if (!adminFind.length) {
+        return res.status(404).json({ message: "admin not found .." })
+    }
+
+    await notificationModel.create({ message, adminID })
+    return res.status(201).json({ message: "notification create successfully .." })
 }
 
 module.exports = { create }
