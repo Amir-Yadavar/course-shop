8f7885489254120c99c6b88d53f07d9e19f50cb6
amir <amiresya@gmail.com>
2025-01-19 15:02:37 +0330

    remove and update department complete ..

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -1,3 +1,4 @@
+const { isValidObjectId } = require("mongoose")
 const { model: departmentModel } = require("./../models/Department")
 
 
@@ -23,10 +24,39 @@ const create = async (req, res) => {
 
 }
 const remove = async (req, res) => {
+    const { id } = req.params
 
+    if (!isValidObjectId(id)) {
+        return res.status(409).json({ message: "id not valid .." })
+    }
+
+    const removeDepartment = await departmentModel.findOneAndDelete({ _id: id })
+
+    if (!removeDepartment) {
+        return res.status(404).json({ message: "this item not found .." })
+    }
+
+    return res.json({ message: "remove department successfully .." })
 }
 const edit = async (req, res) => {
+    const { id } = req.params
 
+    if (!isValidObjectId(id)) {
+        return res.status(409).json({ message: "id not valid .." })
+    }
+
+    const { title } = req.body
+
+    if (!title) {
+        return res.status(409).json({ message: "title is required .." })
+    }
+
+    const updateDepartment = await departmentModel.findOneAndUpdate({ _id: id }, { title })
+
+    if (!updateDepartment) {
+        return res.status(404).json({ message: "this item not found .." })
+    }
+    return res.json({ message: "update department successfully .." })
 }
 
 module.exports = { getAll, create, remove, edit }
